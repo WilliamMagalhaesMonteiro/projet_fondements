b5def047cd6d52acf824db057e790795b49b6dcf
Utilisateur <user@debian.arcahe.ovh>
2021-12-02 16:33:31 +0100

    Fonction de calcul de distance d(a,b) et donc une fonction de valeur absolue aussi

--- Projet_Fondements/main.c
+++ Projet_Fondements/main.c
@@ -8,16 +8,37 @@
 
 
 
-int main(){
+int main(int argc, char *argv[]){
+
+    if (argc != 2) {
+        printf ("Err,must run with {a./out k}\n");
+        exit(1);
+    }
+
+    int k = atoi(argv[1]);
+
+    if (k== 0){
+        printf ("Err, argv[1] must be an integer\n");
+        exit(1);
+    }
+
 
 OBJET *dataset = malloc(sizeof(OBJET) * 50);
 file_to_objet(PATH, dataset);
 
 
+printf("k : %d\n", k);
 
-int k = 5;
 OBJET *seed = malloc(sizeof(OBJET) * k);
 dataset_to_seed(k,dataset,seed);
+
+
+// Le but de l'algo est le suviant:
+// Prendre un objet au pif qui n'est pas l'objet représentatif
+// On calcule toutes les distances à cet objet
+// Si Cette configuration est meilleure que celle avec l'objet représentatif acutel on fait l'échange sinon non
+// on fait ça pour touts les objest.
+
 
 
 
@@ -26,11 +47,7 @@ for (int i = 0; i < 50; i++){
     free(dataset[i].maison);
 }
 
-
 free(seed);
-
-
-
 free(dataset);
 
 return 0;
--- Projet_Fondements/pam.c
+++ Projet_Fondements/pam.c
@@ -1,23 +1,41 @@
 #include "pam.h"
 #include <stdlib.h>
 #include <stdio.h>
+
+
+int abs(int a){ //renvoie la valeur absolue
+    if (a <= 0){ 
+        return -a;
+    }
+    else {
+        return a;
+    }
+} 
+
 int dataset_to_seed(int k,OBJET *dataset, OBJET *seed){
-
-
     //srandom(time(NULL));
     srandom(2);
 
     int r;
-    //OBJET obj;
 
     for (int i = 0; i < k;  i++){
         r = random()%50;
-
-
-
         seed[i] = dataset[r];
     }
 
     return 0;
 
+}
+
+
+
+int d(OBJET a, OBJET b) {   // Renvoie la somme des distances de chaque champs des objets
+
+    int rep = 0;
+   rep += abs(a.courage - b.courage);
+   rep += abs(a.loyauté - b.loyauté);
+   rep += abs(a.sagesse - b.sagesse);
+   rep += abs(a.malice - b.malice);
+
+    return rep;
 }
